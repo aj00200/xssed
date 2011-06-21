3e372f10e6f7127c4ff08f899e9de503d669dd69
= <aj00200@aj00200.org>
2011-06-21 17:54:22 -0500

    Possible fix for bug in javascriptxss/vulnerable.php on line 6

--- javascriptxss/vulnerable.php
+++ javascriptxss/vulnerable.php
@@ -3,7 +3,7 @@ require('../template/head.php');
 
 print <<<END
 <script type="text/javascript">
-var serach_term = "$_GET['q']";
+var serach_term = " $_GET['q'] ";
 </script>
 
 Sorry, we could not find any results for: 
