9bee37a8bf2124858b54b145e22a3b60a0d04e99
= <aj00200@aj00200.org>
2011-06-21 17:57:57 -0500

    Fixed JavaScript variable name

--- javascriptxss/vulnerable.php
+++ javascriptxss/vulnerable.php
@@ -4,11 +4,12 @@ $q = $_GET['q'];
 
 print <<<END
 <script type="text/javascript">
-var serach_term = " $q";
+var search_term = " $q";
 </script>
 
 Sorry, we could not find any results for:
 <script type="text/javascript">document.write(search_term);</script>
 END;
 
+require('../template/foot.php');
 ?>
