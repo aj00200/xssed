000d41000fe4c74032404f473a430b4b93b8cefb
= <aj00200@aj00200.org>
2011-06-21 17:55:36 -0500

    Possible fix for bug in javascriptxss/vulnerable.php on line 6

--- javascriptxss/vulnerable.php
+++ javascriptxss/vulnerable.php
@@ -1,12 +1,13 @@
 <?php
 require('../template/head.php');
+$q = $_GET['q'];
 
 print <<<END
 <script type="text/javascript">
-var serach_term = " $_GET['q'] ";
+var serach_term = " $q";
 </script>
 
-Sorry, we could not find any results for: 
+Sorry, we could not find any results for:
 <script type="text/javascript">document.write(search_term);</script>
 END;
 
