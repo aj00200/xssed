8954651cc9daaff25a202c8adcd26bc6c17650ce
= <aj00200@aj00200.org>
2011-06-21 17:23:48 -0500

    Fixed errors

--- basicxss/vulnerable.php
+++ basicxss/vulnerable.php
@@ -1,6 +1,6 @@
 <?php
-require('../template/head.php')
-echo('Sorry, we could not find any results for: ')
+require('../template/head.php');
+echo('Sorry, we could not find any results for: ');
 echo($_GET['q']);
-require('../template/foot.php')
+require('../template/foot.php');
 ?>
