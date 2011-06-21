999214e45c5357330b2436b900e58f02abda9a51
= <aj00200@aj00200.org>
2011-06-21 17:30:33 -0500

    Corrected excess printing of echos

--- template/foot.php
+++ template/foot.php
@@ -1,2 +1,4 @@
+<?php
 echo(' </body>');
 echo('</html>');
+?>
--- template/head.php
+++ template/head.php
@@ -1,3 +1,4 @@
+<?php
 echo('<!DOCTYPE html>');
 echo('<html>');
 echo(' <head>');
@@ -5,3 +6,4 @@ echo('  <link rel="stylesheet" href="../global.css" type="text/css" />');
 echo('  <title>XSSed</title>');
 echo(' </head>');
 echo(' <body>');
+?>
