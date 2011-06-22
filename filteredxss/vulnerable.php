f069fa020c1219503dd6b03c31887e6f3031d05c
= <aj00200@aj00200.org>
2011-06-21 20:27:00 -0500

    Added Filtered XSS challenge

---
+++ filteredxss/vulnerable.php
@@ -0,0 +1,18 @@
+<?php
+require('../template/head.php');
+$id = str_replace('"', '&quot;', $_GET['id']);
+$creditcard = $_GET['creditcard'];
+$creditcard = preg_replace('/([^\s])([A-Za-z\\"\\\'])/', '', $creditcard);
+
+print <<<END
+<h1>Please Confirm Your Order</h1>
+<div id="creditcard">$creditcard</div>
+
+<script type="text/javascript">
+var q = "$id";
+document.write(q);
+</script>
+END;
+
+require('../template/foot.php');
+?>
